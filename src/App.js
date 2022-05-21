b51048b895696a7f619f9a6c0b0a5c92ff1dab44
Sony <sohni2012@gmail.com>
2022-05-21 14:16:59 +0530

    basic react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>InBar</h1>
+       <button>Connet Wallet</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
